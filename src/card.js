07b702745e2e48b3042173b1005d7f51f7bd8d4d
rland12 <rland7596@gmail.com>
2023-02-28 22:19:39 -0500

    add images and card

---
+++ src/card.js
@@ -0,0 +1,11 @@
+function Card(prop){
+    //replace home with heart, put to top right of img
+return(
+    <div>
+        <img src="images\heart.png" className="favorite"></img>
+
+    </div>
+);
+}
+
+export default Card;

edd2d344276691d19a19c09a40d31c7359062f40
rland12 <rland7596@gmail.com>
2023-03-08 22:52:12 -0500

    add react responsive carousel

--- src/Image_Carousel.js
+++ src/Image_Carousel.js
@@ -1,8 +1,21 @@
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 function Image_Carousel(){
     return(
-        <div className="image-container">
-            <img src="images\home-exterior.jpg" className="location-img"></img>
-        </div>
+        <div className="carousel-wrapper image-container">
+        <Carousel autoPlay>
+            <div>
+                <img src="images\home-exterior.jpg" className="location-img"/>
+            </div>
+            <div>
+                <img src="images\bedroom.jpg" />
+            </div>
+            <div>
+                <img src="images\livingroom.jpg" />
+            </div>
+        </Carousel>
+    </div>
+        
     );
 }
 
